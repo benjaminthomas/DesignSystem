8d74e9646abdd733544ce98529b4b044f4402cd9
Benjamin <benjaminthomas@users.noreply.github.com>
2025-01-08 12:35:39 +0530

    Test

--- components/navigations/navitems.tsx
+++ components/navigations/navitems.tsx
@@ -7,20 +7,20 @@ import React from "react";
 type PrimaryNavItemsProps = (typeof primaryLinks)[number];
 
 export default function PrimaryNavItems({ label, href }: PrimaryNavItemsProps) {
-  const pathname = usePathname();
-  return (
-    <li>
-      <Link
-        className={`relative text-primary-500 p-3 leading-relaxed block ${
-          pathname === `${href}` ? "text-primary-700 font-bold" : ""
-        }`}
-        href={href}
-      >
-        {pathname === `${href}` && (
-          <span className="absolute left-0 top-0 bg-orange z-0 h-full w-full"></span>
-        )}
-        <span className="relative z-10">{label}</span>
-      </Link>
-    </li>
-  );
+    const pathname = usePathname();
+    return (
+        <li>
+            <Link
+                className={`relative text-primary-500 p-3 leading-relaxed block ${
+                    pathname === `${href}` ? "text-primary-600 font-bold" : ""
+                }`}
+                href={href}
+            >
+                {pathname === `${href}` && (
+                    <span className="absolute left-0 top-0 bg-orange z-0 h-full w-full"></span>
+                )}
+                <span className="relative z-10">{label}</span>
+            </Link>
+        </li>
+    );
 }
